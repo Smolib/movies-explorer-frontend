2bb23cd416fae54b2f8126749bac3dfa06b5e424
Mariya Kulakovskaya <kulakovskaya-mariya@yandex.ru>
2022-09-09 16:18:18 +0300

    feat: добавить блок с карточками фильмов

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,8 +1,14 @@
-function MoviesCard() {
-    return (
-      <>
-      </>
-    );
-  }
-  
-  export default MoviesCard;
+import "./MoviesCard.css";
+
+function MoviesCard({ title, time, image, nameOfButton, textOfButton }) {
+  return (
+    <arcticle className="movie-card">
+      <h2 className="movie-card__name">{title}</h2>
+      <span className="movie-card__time">{time}</span>
+      <img className="movie-card__image" src={image} alt={title}/>
+      <button className={`movie-card__button movie-card__button_type_${nameOfButton}`}>{textOfButton ? textOfButton : ''}</button>
+    </arcticle>
+  );
+}
+
+export default MoviesCard;
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,7 +1,22 @@
+import "./MoviesCardList.css";
+import MovieCard from '../MoviesCard/MoviesCard';
+import image1 from '../../images/movies/movie1.jpg';
+import image2 from '../../images/movies/movie2.jpg';
+import image3 from '../../images/movies/movie3.jpg';
+
 function MoviesCardList() {
     return (
-      <>
-      </>
+      <section className="movies-list">
+        <MovieCard title='В погоне за Бенкси' time="27 минут" image={image1} nameOfButton="saved"/>
+        <MovieCard title='В погоне за Кенкси' time="26 минут" image={image2} nameOfButton="saved"/>
+        <MovieCard title='В погоне за Венкси' time="25 минут" image={image3} nameOfButton="save" textOfButton="Сохранить"/>
+        <MovieCard title='В погоне за Бенкси' time="27 минут" image={image1} nameOfButton="saved"/>
+        <MovieCard title='В погоне за Кенкси' time="26 минут" image={image2} nameOfButton="save" textOfButton="Сохранить"/>
+        <MovieCard title='В погоне за Венкси' time="25 минут" image={image3} nameOfButton="delete"/>
+        <MovieCard title='В погоне за Бенкси' time="27 минут" image={image1} nameOfButton="saved"/>
+        <MovieCard title='В погоне за Кенкси' time="26 минут" image={image2} nameOfButton="save" textOfButton="Сохранить"/>
+        <MovieCard title='В погоне за Венкси' time="25 минут" image={image3} nameOfButton="delete"/>
+      </section>
     );
   }
   
